af894c0d0c8aab275e5a62e916d0c758d10164e2
yael-shalom <yael7147757@gmail.com>
2024-09-23 00:41:33 +0300

    handleErrors -> middlewares

---
+++ middlewares/handleErrors.js
@@ -0,0 +1,14 @@
+export function pageNotFound(req,res,next){
+    const error = new Error(`the page is Not Found`);
+    error.status = 404;
+    next(error);
+}
+
+export function serverNotFound(err,req,res,next){
+    res.status=err.status||500;
+    res.json({
+        error:{
+            message:err.message
+        }
+    })
+}
