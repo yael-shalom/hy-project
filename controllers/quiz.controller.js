081640d13055a997ffed347342d5a0d694514f50
hodaya <hp0504124950@gmail.com>
2024-09-23 23:40:57 +0300

    good?

--- controllers/quiz.controller.js
+++ controllers/quiz.controller.js
@@ -4,21 +4,22 @@
 // import path from 'path';
 
 
-// צריך את {  כמו שהיא עשתה?}
-import Quiz from '../models/quiz.model.js'; // ודא שיש לך את המודל המתאים
-
 
 export async function getAllQuiz(req, res, next) {
+
     try {
         // קבלת כל המבחנים ממאגר הנתונים
         const quizzes = await Quiz.find();
 
         // החזרת המבחנים
         res.json(quizzes);
-    } catch (error) {
+    } 
+    
+    catch (error) {
         console.error('Error fetching quizzes:', error);
         res.status(500).json({ message: 'Server error' });
     }
+
 }
 
 
