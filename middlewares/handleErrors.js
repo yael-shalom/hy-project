f274b6dabcd50013d8f63cd2d8fd887f44ef6c3d
yael-shalom <yael7147757@gmail.com>
2024-09-26 00:04:52 +0300

    fix serverNotFound status bug

--- middlewares/handleErrors.js
+++ middlewares/handleErrors.js
@@ -1,14 +1,14 @@
-export function pageNotFound(req,res,next){
+export function pageNotFound(req, res, next) {
     const error = new Error(`the page is Not Found`);
     error.status = 404;
     next(error);
 }
 
-export function serverNotFound(err,req,res,next){
-    res.status=err.status||500;
+export function serverNotFound(err, req, res, next) {
+    res.status(err.status || 500);
     res.json({
-        error:{
-            message:err.message
+        error: {
+            message: err.message
         }
     })
 }
