5247233b1a87d94df616a22c2946df0d59dbbdc9
yael-shalom <yael7147757@gmail.com>
2024-09-26 00:07:06 +0300

    connect to db & remove unnecessary imports

--- app.js
+++ app.js
@@ -1,39 +1,42 @@
-import express, { json, urlencoded } from 'express';
+import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 
 // my routes
-// import userRouter from './routes/user.route.js';
+import userRouter from './routes/user.route.js';
 // import categoryRouter from './routes/category.route.js';
 // import recipeRouter from './routes/recipe.route.js';
 
-import { pageNotFound, serverNotFound } from './middlewares/handleErrors.js'; 
+import { pageNotFound, serverNotFound } from './middlewares/handleErrors.js';
 import 'dotenv/config';
-import './config/db.js';
+import { DBconnect } from './config/db.js';
+
+DBconnect();
 
 //יוצר שרת
 const app = express();
 
 //יוצרים את ה-body של ה-req
-app.use(json()); 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //עושה הדפסות שיהיה קל לדבג
-app.use(morgan("dev")); 
+app.use(morgan("dev"));
 
 // app.use(cors({origin:"קליינט אמיתי"})); 
 //מאפשר גישה לכולם לפני ההגשה לשנות לנ"ל
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send("wellcome");
-});
-// app.use("/users", userRouter);
+// app.get('/', (req, res) => {
+//   res.send("wellcome");
+// });
+
+app.use("/users", userRouter);
 // app.use("/categories", categoryRouter);
 // app.use("/recipes",recipeRouter);
 
 //בשביל להעלות תמונות
-app.use('/images',express.static('uploads'))
+app.use('/images', express.static('uploads'))
 
 //של השגיאות, אם לא מצא נתיב יגיע לזה
 app.use(pageNotFound);
