915a74ef9145d59edce4f9d95047d894f471e05a
yael-shalom <yael7147757@gmail.com>
2024-09-26 00:06:18 +0300

    remove generateToken

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,20 +1,19 @@
 import mongoose from 'mongoose';
-import path from 'path';
 
 import { User } from '../models/user.model.js'
 
 export async function signIn(req, res, next) {
     const email = req.body.email;
     const password = req.body.password;
-    const user = await User.findOne(email); //????????????????????????????????????????????????????
+    const user = await User.findOne({ email }); //במקום email:email רק email כי זה באותו שם
     if (user) {
         bcrypt.compare(password, user.password, (err, isSame) => {
             if (err)
                 return next(new Error(err.message));
             if (isSame) {
-                const token = generateToken(user);
+                // const token = generateToken(user);
                 user.password = "****";
-                return res.send({ user, token });
+                return res.json(user);
             }
             return next({ message: 'Auth Failed-user is not found', status: 401 })
         })
@@ -27,12 +26,38 @@ export async function signIn(req, res, next) {
 export async function signUp(req, res, next) {
     const { name, email, password } = req.body;
     try {
-        const user = new User({ name, email, password, });//??????????????????????????
+        const user = new User({ name, email, password });//??????????????????????????
         await user.save();
-        const token = generateToken(user);
+        // const token = generateToken(user);
         user.password = "****";
-        return res.status(201).json({ user, token });
-    } catch (error) {
+        return res.status(201).json(user);
+    } catch (error) {        
         return next({ message: error.message, status: 409 })
+    }
+}
+
+export async function updateUser(req, res, next) {
+
+}
+
+export async function deleteUser(req, res, next) {
+
+}
+
+/////////////////////////////////
+export async function getAllUsers(req, res, next) {
+    try {
+        console.log("gooddddddddddddddddd");
+
+        // קבלת כל המבחנים ממאגר הנתונים
+        const users = await User.find();
+        console.log(users);
+
+        // החזרת המבחנים
+        res.json(users);
+    }
+
+    catch (error) {
+        next({ message: error.message, status: 500 })
     }
 }
