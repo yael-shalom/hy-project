220733bc50c40cf1fb5d1bcd1f98f17228cf08f7
yael-shalom <yael7147757@gmail.com>
2024-10-06 15:39:51 +0300

    change DB_URL to MONGODB_URI

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export function DBconnect()
 {
-    const db = process.env.DB_URL || 'mongodb://localhost:27017/';
+    const db = process.env.MONGODB_URI || 'mongodb://localhost:27017/';
     mongoose.connect(db).then(() => console.log("mongo DB connected"))
     .catch(error => console.log("mongo DB error", error.message));
 }
