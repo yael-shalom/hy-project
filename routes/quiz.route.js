9383c0a559c74e2e4c33f0e0eecc2a68aa476dc9
yael-shalom <yael7147757@gmail.com>
2024-10-10 22:12:57 +0300

    remove addquiz from route

--- routes/quiz.route.js
+++ routes/quiz.route.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import { getAllQuizzes, addQuiz, getQuizById, updateQuiz } from '../controllers/quiz.controller.js'
+import { getAllQuizzes, addQuiz, getQuizById, updateQuiz, getQuizByUserId } from '../controllers/quiz.controller.js'
 import { isAuth } from '../middlewares/auth.js';
 
 const router = express.Router();
 router.get('/', getAllQuizzes);
 router.get('/:id', getQuizById);
-router.post('/addquiz', addQuiz);
+router.get('/user/:id', getQuizByUserId);//להוסיף isAuth
+router.post('/', addQuiz);
 router.patch('/:id',isAuth, updateQuiz);
 
 export default router
