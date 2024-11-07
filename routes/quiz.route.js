7648a1bd042874af1eb08cc291bc277146c58256
yael-shalom <yael7147757@gmail.com>
2024-11-07 19:50:55 +0200

    adding isAuth & delete rout

--- routes/quiz.route.js
+++ routes/quiz.route.js
@@ -1,12 +1,13 @@
 import express from 'express'
-import { getAllQuizzes, addQuiz, getQuizById, updateQuiz, getQuizByUserId } from '../controllers/quiz.controller.js'
+import { getAllQuizzes, addQuiz, getQuizById, updateQuiz, getQuizByUserId, deleteQuiz, getImage } from '../controllers/quiz.controller.js'
 import { getUser, isAuth } from '../middlewares/auth.js';
 
 const router = express.Router();
 router.get('/', getUser, getAllQuizzes);
 router.get('/:id', getUser, getQuizById);
 router.get('/owner/:id', isAuth, getQuizByUserId);
-router.post('/', addQuiz);//להוסיף isAuth
-router.patch('/:id', updateQuiz);//להוסיף isAuth
+router.post('/', isAuth, addQuiz);//להוסיף isAuth
+router.patch('/:id', isAuth, updateQuiz);//להוסיף isAuth
+router.delete('/:id', isAuth, deleteQuiz);//להוסיף isAuth
 
 export default router
